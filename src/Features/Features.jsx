4a4eea34dfc5a8bc070798a7f9f23ccd4a4fa86f
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-04-10 10:21:46 +0600

    json file updated

--- src/Features/Features.jsx
+++ src/Features/Features.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { CommandLineIcon, ComputerDesktopIcon, CreditCardIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid'
 const Features = ({information}) => {
     const {title, info, pic} = information
     console.log(pic)
     return (
         <div>
             <div className='flex justify-center'>
-                    <div className='mr-5 duration-500 hover:bg-indigo-200 hover:rounded-xl'>
+                    <div className='mr-5 duration-500 hover:bg-indigo-200 hover:rounded-xl flex flex-col items-center'>
                         <img className='w-6' src={pic} alt="" />
                         {/* <CommandLineIcon className='w-10 fill-indigo-400'></CommandLineIcon> */}
                         <h1 className='font-bold'>{title}</h1>
